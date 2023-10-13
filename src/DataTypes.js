c43c1336c3befb318f14fc8d34daf9075260bff7
ilker <57532341+ilkerozgen@users.noreply.github.com>
2023-10-13 10:53:34 +0300

    Data Types

---
+++ src/DataTypes.js
@@ -0,0 +1,23 @@
+import './DataTypesStyles.css';
+import bar from './images/Data Types.png'
+import "@fontsource/space-grotesk";
+import "@fontsource/space-grotesk/700.css";
+
+function DataTypes() {
+  return (
+    <div>
+      <div className='center'>
+        <div className='header'>
+          For every data type
+        </div>
+      </div>
+      <div className='center'>
+        <div className='container'>
+          <img src={bar} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default DataTypes;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import DataTypes from './DataTypes';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <DataTypes />
+  </React.StrictMode>
+);
