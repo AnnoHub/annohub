c8d6b37add3f532a01b804870a2a68a440f1058c
Dağhan Ünal <daghanunal20@gmail.com>
2024-03-26 22:33:39 +0300

    fix

--- src/components/footer/FooterDocs.js
+++ src/components/footer/FooterDocs.js
@@ -6,7 +6,7 @@ function Docs() {
         <div>
             <p className='footer-title'>Docs</p>
             <u1 className="doc-links">
-                <li><a class='footer-text' href={`${process.env.PUBLIC_URL}/docs/T2331_Analysis_and_Requirements_Report.pdf.pdf`} target="_blank" rel="noopener noreferrer">Analysis and Requirements Report</a></li>
+                <li><a class='footer-text' href={`${process.env.PUBLIC_URL}/docs/T2331_Analysis_and_Requirements_Report.pdf`} target="_blank" rel="noopener noreferrer">Analysis and Requirements Report</a></li>
                 <li><a class='footer-text' href={`${process.env.PUBLIC_URL}/docs/T2331_Detailed_Design_report_1.pdf`} target="_blank" rel="noopener noreferrer">Detailed Design Report</a></li>
                 <li><a class='footer-text' href={`${process.env.PUBLIC_URL}/docs/T2331_Project_Specification_Document.pdf`} target="_blank" rel="noopener noreferrer">Project Specifications Report</a></li>
             </u1>
