3654a36e6d060e51d9a1b0f2ef6badf6df00ee88
Dağhan Ünal <daghanunal20@gmail.com>
2024-03-26 18:27:29 +0300

    reports added

--- src/components/footer/FooterDocs.js
+++ src/components/footer/FooterDocs.js
@@ -3,10 +3,15 @@ import './Footer.css';
 
 function Docs() {
     return (
-      <div>
-        <p class='footer-title'>Docs</p>
-      </div>
-    )
-  }
-  
+        <div>
+            <p className='footer-title'>Docs</p>
+            <u1 className="doc-links">
+                <li><a class='footer-text' href={`${process.env.PUBLIC_URL}/docs/T2331_Analysis_and_Requirements_Report.pdf.pdf`} target="_blank" rel="noopener noreferrer">Analysis and Requirements Report</a></li>
+                <li><a class='footer-text' href={`${process.env.PUBLIC_URL}/docs/T2331_Detailed_Design_report_1.pdf`} target="_blank" rel="noopener noreferrer">Detailed Design Report</a></li>
+                <li><a class='footer-text' href={`${process.env.PUBLIC_URL}/docs/T2331_Project_Specification_Document.pdf`} target="_blank" rel="noopener noreferrer">Project Specifications Report</a></li>
+            </u1>
+        </div>
+    );
+}
+
 export default Docs
